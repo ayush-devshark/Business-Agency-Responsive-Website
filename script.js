3e596868047894e6cf7e77dd58901bed3e599c0c
ayush-devshark <ayushagarwal.dev@gmail.com>
2021-04-19 14:44:42 +0530

    Adding About us and video feature

--- script.js
+++ script.js
@@ -7,3 +7,33 @@ menu.addEventListener('click', () => {
     menu.classList.toggle('change');
 });
 //End of Navbar
+
+// Section 2 video
+const video = document.querySelector('.video');
+const btn = document.querySelector('.buttons i');
+const bar = document.querySelector('.video-bar');
+
+const playPause = () => {
+    if (video.paused) {
+        video.play();
+        btn.className = 'far fa-pause-circle';
+        video.style.opacity = `.7`;
+    } else {
+        video.pause();
+        btn.className = 'far fa-play-circle';
+        video.style.opacity = '.3';
+    }
+};
+
+btn.addEventListener('click', playPause);
+
+video.addEventListener('timeupdate', () => {
+    // console.log(video.currentTime, video.duration);
+    const barwidth = video.currentTime / video.duration;
+    bar.style.width = `${barwidth * 100}%`;
+    if (video.ended) {
+        btn.className = 'far fa-play-circle';
+        video.style.opacity = `0.3`;
+    }
+});
+// End of Section 2 video
